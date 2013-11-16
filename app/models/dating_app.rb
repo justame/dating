0bfd1c2bb6adb099649a806496a5aab429afe975
justame <justame@gmail.com>
2013-11-16 21:28:25 +0200

    working basic example

---
+++ app/models/dating_app.rb
@@ -0,0 +1,15 @@
+class DatingApp
+  @@APP_ID = "191719354235098"
+  @@APP_SECRET = "c85118212805cc76c3c4ef6b17c6eaf7"
+  @@app_oauth = nil
+  
+  def self.APP_ID
+    @@APP_ID
+  end
+
+  def self.APP_SECRET
+    @@APP_SECRET
+  end
+
+
+end
